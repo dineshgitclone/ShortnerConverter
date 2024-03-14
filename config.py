7598ca8e4613e1964ec8c6d9b9eb3207003f5239
dineshgitclone <116178552+dineshgitclone@users.noreply.github.com>
2024-03-14 09:29:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertor")
 DATABASE_URL = os.environ.get(
     "DATABASE_URL", None
 )  # mongodb uri from https://www.mongodb.com/
-OWNER_ID = int(os.environ.get("OWNER_ID"))  # id of the owner
+OWNER_ID = int(os.environ.get("OWNER_ID", "1608576332"))  # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
 #  Optionnal variables
